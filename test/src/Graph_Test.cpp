c6a30bb477875fa5e03744a4b5fd2b86e1f95e24
maucarrui <maucarrui@ciencias.unam.mx>
2020-12-03 19:57:07 -0600

    Graph: New unit tests.

--- test/src/Graph_Test.cpp
+++ test/src/Graph_Test.cpp
@@ -44,6 +44,50 @@ TEST_F(GRAPHTest, constructor) {
     EXPECT_EQ(500 * 1000, G.getNumEdges());
 }
 
+TEST_F(GRAPHTest, getCapacityOf) {
+    DAO dao = DAO("../sql/database.db");
+    dao.openDB();
+
+    std::vector<Worker> workers = dao.getWorkers();
+    std::vector<Task>   tasks   = dao.getTasks();
+
+    Graph G = Graph(workers, tasks, dao);
+
+    double test1 = dao.getCapacity(2, 2);
+    double test2 = dao.getCapacity(60, 700);
+    double test3 = dao.getCapacity(240, 20);
+    
+    double supp_test1 = G.getCapacityOf(2, 2);
+    double supp_test2 = G.getCapacityOf(60, 700);
+    double supp_test3 = G.getCapacityOf(240, 20);
+
+    EXPECT_EQ(test1, supp_test1);
+    EXPECT_EQ(test2, supp_test2);
+    EXPECT_EQ(test3, supp_test3);
+}
+
+TEST_F(GRAPHTest, getCostOf) {
+    DAO dao = DAO("../sql/database.db");
+    dao.openDB();
+
+    std::vector<Worker> workers = dao.getWorkers();
+    std::vector<Task>   tasks   = dao.getTasks();
+
+    Graph G = Graph(workers, tasks, dao);
+
+    double test1 = dao.getCost(2, 2);
+    double test2 = dao.getCost(60, 700);
+    double test3 = dao.getCost(240, 20);
+    
+    double supp_test1 = G.getCostOf(2, 2);
+    double supp_test2 = G.getCostOf(60, 700);
+    double supp_test3 = G.getCostOf(240, 20);
+
+    EXPECT_EQ(test1, supp_test1);
+    EXPECT_EQ(test2, supp_test2);
+    EXPECT_EQ(test3, supp_test3);
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
 
